9af8987a906fb9fcf9841b4fd11243ca688caa17
Thibault Chardigny <thibault.chardigny@gmail.com>
2020-11-12 12:59:59 +0100

    12112020 13:00

--- js/ajax.js
+++ js/ajax.js
@@ -31,21 +31,28 @@ function ajaxGet(url, callback)
 // Exécute un appel AJAX POST
 // Prend en paramètres l'URL cible, la donnée à envoyer et la fonction callback appelée en cas de succès
 // Le paramètre isJson permet d'indiquer si l'envoi concerne des données JSON
-function ajaxPost(url, data, callback, isJson) {
+
+function ajaxPost(url, data, callback, isJson) 
+{
     var req = new XMLHttpRequest();
     req.open("POST", url);
-    req.addEventListener("load", function () {
-        if (req.status >= 200 && req.status < 400) {
+    req.addEventListener("load", function () 
+    {
+        if (req.status >= 200 && req.status < 400) 
+        {
             // Appelle la fonction callback en lui passant la réponse de la requête
             callback(req.responseText);
-        } else {
+        } else 
+        {
             console.error(req.status + " " + req.statusText + " " + url);
         }
     });
-    req.addEventListener("error", function () {
+    req.addEventListener("error", function () 
+    {
         console.error("Erreur réseau avec l'URL " + url);
     });
-    if (isJson) {
+    if (isJson) 
+    {
         // Définit le contenu de la requête comme étant du JSON
         req.setRequestHeader("Content-Type", "application/json");
         // Transforme la donnée du format JSON vers le format texte avant l'envoi
@@ -56,15 +63,18 @@ function ajaxPost(url, data, callback, isJson) {
 
 // Fonction pour détecter que localeStorage est bien supporté par le navigateur et disponible :
 
-    function storageAvailable(type) {
-    try {
+function storageAvailable(type) 
+{
+    try 
+    {
         var storage = window[type],
             x = '__storage_test__';
         storage.setItem(x, x);
         storage.removeItem(x);
         return true;
     }
-    catch(e) {
+    catch(e) 
+    {
         return e instanceof DOMException && (
             // everything except Firefox
             e.code === 22 ||
@@ -79,3 +89,5 @@ function ajaxPost(url, data, callback, isJson) {
             storage.length !== 0;
     }
 }
+
+
